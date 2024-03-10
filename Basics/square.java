a3578d4d325837b2735660dba1bdc5044764c38b
Java-Sculptor <deepaksonavale29@gmail.com>
2024-03-10 21:53:01 +0530

    find square of any integer

---
+++ Basics/square.java
@@ -0,0 +1,13 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class square {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int n=sc.nextInt();
+        System.out.println("Square of "+n+" = "+(n*n));
+    }
+    
+}

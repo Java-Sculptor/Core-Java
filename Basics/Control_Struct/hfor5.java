0e236ce15e436dad0e6a6c8fd0a00036b6f23196
Java-Sculptor <deepaksonavale29@gmail.com>
2024-03-12 21:28:23 +0530

    print hello 5 times using for loop

---
+++ Basics/Control_Struct/hfor5.java
@@ -0,0 +1,13 @@
+package Basics.Control_Struct;
+
+public class hfor5 {
+    public static void main(String[] args) {
+        
+    
+    for(int i=0;i<5;i++)
+    {
+        System.out.println("Hello");
+    }
+    
+}
+}

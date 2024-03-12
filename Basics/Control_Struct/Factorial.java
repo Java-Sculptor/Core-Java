70aa36fc95e624fdaee549c9bee3cb5a3d2f2b8e
Java-Sculptor <deepaksonavale29@gmail.com>
2024-03-12 21:35:31 +0530

    Find fact using for loop

---
+++ Basics/Control_Struct/Factorial.java
@@ -0,0 +1,18 @@
+package Basics.Control_Struct;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number :");
+        int n=sc.nextInt();
+        int f=1;
+        for(int i=1;i<=n;i++)
+        {
+            f*=i;
+        }
+        System.out.println("Factorial = "+f);
+    }
+    
+}

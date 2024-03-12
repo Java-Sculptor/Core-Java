3b14c738828492290f9b997a0e3e0540f1aaec6d
Java-Sculptor <deepaksonavale29@gmail.com>
2024-03-12 20:48:52 +0530

    float typecast to int

---
+++ Basics/Cast.java
@@ -0,0 +1,13 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Cast {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float f=sc.nextFloat();
+        int i=(int)f;
+        System.out.println(i);
+    }
+    
+}

bdd68e42368f7f4007a19bd0272b7f3b678387ca
Java-Sculptor <deepaksonavale29@gmail.com>
2024-03-18 21:35:21 +0530

    print "Computer" 5 times using for loop

---
+++ Basics/Control_Struct/for5.java
@@ -0,0 +1,11 @@
+package Basics.Control_Struct;
+
+public class for5 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("Computer");
+        }
+    }
+    
+}

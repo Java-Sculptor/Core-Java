1f713084d085c7bdffd89f82370af2b38cc8c35f
Java-Sculptor <deepaksonavale29@gmail.com>
2024-03-18 22:06:55 +0530

    hii r=5 c=2

---
+++ Basics/Control_Struct/Pattern/hiiptn.java
@@ -0,0 +1,15 @@
+package Basics.Control_Struct.Pattern;
+
+public class hiiptn {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                System.out.print("hii ");
+            }
+            System.out.println();
+        }
+    }
+    
+}

b648269ecf63cb5950585652f35c0049491f224b
Java-Sculptor <deepaksonavale29@gmail.com>
2024-03-20 21:38:47 +0530

    Decimal to binary conversion

---
+++ Whileloop/Binary.java
@@ -0,0 +1,28 @@
+package Whileloop;
+
+import java.util.Scanner;
+
+public class Binary {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any numm:");
+        int d=sc.nextInt();
+        String b=Binary(d);
+        System.out.println(b);
+    }
+    public static String Binary(int d)
+    {
+        StringBuilder b = new StringBuilder();
+		        if (d == 0) {
+		            return "0";
+		        }
+		        while (d!=0) {
+		            int r = d % 2;
+		            b.insert(0, r); // Inserting the remainder at the beginning of the string
+		            d /= 2;
+		        }
+		        return b.toString();
+		    
+    }
+    
+}

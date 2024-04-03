24acb7978fd93dfb79acd610350d6a973fa28a86
Java-Sculptor <deepaksonavale29@gmail.com>
2024-04-03 23:23:03 +0530

    Anonymous inner class as method argument

---
+++ Interface/MrthodArgu.java
@@ -0,0 +1,20 @@
+package Interface;
+
+interface MyInterface
+{
+    void display();
+}
+public class MrthodArgu {
+    public static void main(String[] args) {
+        Method(new MyInterface() {
+            @Override
+            public void display()
+            {
+                System.out.println("Anonymous inner class as method argument");
+            }
+        });
+    }
+    static void Method(MyInterface obj) {
+        obj.display();
+    }
+}

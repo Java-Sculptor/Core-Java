be96d42dde47ef1c0b31129cb5e6ede27a1814fa
Java-Sculptor <deepaksonavale29@gmail.com>
2024-04-03 20:42:38 +0530

    local innner class defines under method and var of that method should be final

--- NestedClass/LocalInner.java
+++ NestedClass/LocalInner.java
@@ -3,11 +3,13 @@ package NestedClass;
 public class LocalInner {
     public void Krma()
     {
+        final int k=6;
         class Inner
         {
             public void show(int m,int n)
             {
             System.out.println("Sum = "+(m+n));
+            System.out.println("Final var of method where we declare local inner class k = "+k);
             }
         }
         Inner i=new Inner();

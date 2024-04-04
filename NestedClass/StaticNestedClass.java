236af89986db32a4310a45ae32a09ee022a8f559
Java-Sculptor <deepaksonavale29@gmail.com>
2024-04-04 14:07:16 +0530

    static nested class obj not strongly associated with outer class obj

---
+++ NestedClass/StaticNestedClass.java
@@ -0,0 +1,26 @@
+package NestedClass;
+
+import NestedClass.Outter.Nested;
+
+class Outter
+{
+    static class Nested
+    {
+        public static void m1()
+        {
+            System.out.println("Static nested class static method");
+        }
+
+        public  void m2()
+        {
+            System.out.println("Static nested class non-static method");
+        }
+    }
+}
+public class StaticNestedClass {
+    public static void main(String[] args) {
+        Nested.m1();
+        Nested n=new Nested();
+        n.m2();
+    }
+}

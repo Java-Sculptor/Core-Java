45b2f1d6cc02b04a216e7d682c0480ae1e58f246
Java-Sculptor <deepaksonavale29@gmail.com>
2024-04-05 21:06:12 +0530

    string == operator (mem loc)and .equals() method(content)

---
+++ NestedClass/Strings/Compare.java
@@ -0,0 +1,16 @@
+package NestedClass.Strings;
+
+public class Compare {
+    public static void main(String []args)
+    {
+        String s1=new String("Deepak");
+        String s2=new String("Deepak");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+        String s3="Advait";
+        String s4="Advait";
+        System.out.println(s3==s4);
+        System.out.println(s3.equals(s4));
+         
+    }
+}

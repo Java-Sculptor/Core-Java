1d508c2c0db961aad50bfc9c5112eddbcadbdf7c
Java-Sculptor <deepaksonavale29@gmail.com>
2024-04-13 01:34:48 +0530

    Implements Comparator interface by LE and stream() method

--- JDK8/ComparatorInterfaceByFuncI.java
+++ JDK8/ComparatorInterfaceByFuncI.java
@@ -3,6 +3,8 @@ package JDK8;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ComparatorInterfaceByFuncI {
     public static void main(String[] args) {
@@ -16,5 +18,8 @@ public class ComparatorInterfaceByFuncI {
 		Comparator <Integer> c=(I1,I2)->(I1<I2)?-1:(I1>I2)?1:0;
         Collections.sort(l,c);
 		System.out.println("Acending Order : "+l);
+        l.stream().forEach(System.out::println);
+        List <Integer> l2=l.stream().filter(i->i%2==0).collect(Collectors.toList());
+        System.out.println(l2);
     }
 }
